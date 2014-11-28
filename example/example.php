410b9d5621cf0e6e6fc35add10b4270ec617087c
Oleksandr Knyga <oleksandrknyga@gmail.com>
2014-11-28 15:48:31 +0200

    example php

--- example/example.php
+++ example/example.php
@@ -1,22 +1,12 @@
 <?php
 
-<<<<<<< HEAD
 include('../vendor/autoload.php');
 
 try {
 	// Instantiate the class
-	$code = 12313132;//your real code
+	$code = 131231;//your real code
 	$text2pdf = new ConvertAPI\Text2Pdf($code);
 	// Convert the file
-=======
-// Include the required concrete conversion class
-include('../Text2PDF.php');
-
-try {
-// Instantiate the class
-	$text2pdf = new ConvertAPI\Text2Pdf();
-// Convert the file
->>>>>>> 5b4972ba305a6a0362d6fa7198cfc62e824ecb57
 	$text2pdf->convert(__DIR__.'/input.txt', __DIR__.'/output.pdf');
 } catch (Exception $e) {
 	var_dump($e->getMessage());
